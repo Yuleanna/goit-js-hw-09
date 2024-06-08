8a2d7a773c0d4c6931bc5a8a539baa45bd31b29d
Julia <157515283+Yuleanna@users.noreply.github.com>
2024-06-08 13:59:54 +0300

    fixed

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -32,7 +32,7 @@ form.addEventListener('input', event => {
 
 form.addEventListener('submit', event => {
   event.preventDefault();
-  if (!emailInput.value || !messageInput.value) {
+  if (!emailInput.value.trim() || !messageInput.value.trim()) {
     alert('Fill please all fields');
     return;
   }
